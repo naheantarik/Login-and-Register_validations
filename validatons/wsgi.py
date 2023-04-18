41621878e4bb3f449d882de86f8e75067d0ea130
naheantarik <nahean.swe@gmail.com>
2023-04-19 04:49:21 +0600

    Inital commit

--- validatons/wsgi.py
+++ validatons/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for validations project.
+WSGI config for validatons project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'validations.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'validatons.settings')
 
 application = get_wsgi_application()
