b6e7a14bfa0f2e67ac2df71125e2fe2dc495fd97
naheantarik <nahean.swe@gmail.com>
2023-04-19 05:36:45 +0600

    Initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
